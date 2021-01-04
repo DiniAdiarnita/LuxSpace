9ba7c0240350c2a988ede9fd5eb1696fe444e612
DiniAdiarnita <diniiadiar@gmail.com>
2021-01-04 21:45:23 +0700

    Add cart page

--- src/js/slider.js
+++ src/js/slider.js
@@ -8,18 +8,13 @@ for (let index = 0; index < sliders.length; index++) {
   const items = slider.querySelectorAll(".slider .item");
   const preview = slider.querySelector("div > .preview");
 
-  for (let item = 0; index < items.length; item++) {
-    const itemTrigger = items[index];
+  for (let item = 0; item < items.length; item++) {
+    const itemTrigger = items[item];
 
     itemTrigger.addEventListener("click", function () {
-      const dataImg = this.attributes?.["data-img"]?.value;
-      for (
-        let eachItemTrigger = 0;
-        eachItemTrigger < items.length;
-        eachItemTrigger++
-      ) {
+      const dataImg = this.attributes?.['data-img']?.value;
+      for (let eachItemTrigger = 0; eachItemTrigger < items.length; eachItemTrigger++) {
         const triggerNeedToRemoved = items[eachItemTrigger];
-
         removeClass(triggerNeedToRemoved, "selected");
       }
       addClass(itemTrigger, "selected");
